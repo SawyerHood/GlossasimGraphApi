195be26a8e50b3f5a80addfe22e3575d2cbda895
unknown <sawyerjhood@gmail.com>
2016-03-17 15:12:01 -0400

    Accepts post requests

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from flask import Flask
+from flask import request
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+@app.route('/trigger', methods=['POST'])
+def trigger():
+    if request.method == 'POST':
+        print request.json
+        return str(request.json)
+    else:
+        return 'Not supported'
 
 if __name__ == '__main__':
     app.run()
