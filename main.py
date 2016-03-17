dd1aff48ed1a0f74eba894e01af524a076a75e21
Sawyer Hood <sawyerjhood@gmail.com>
2016-03-17 20:34:24 +0000

    Body Language implemented

--- main.py
+++ main.py
@@ -11,10 +11,11 @@ def root():
     return 'Welcome to the GlossasimGraphApi!'
 
 def create_graph(data, graph_type='hbar', title=''):
-    import pdb; pdb.set_trace()
     graph = None
     if graph_type == 'hbar':
         graph = pygal.HorizontalBar()
+    elif graph_type == 'pie':
+        graph = pygal.Pie()
     graph.title = title
     sorted_value_tuples = sorted(data.items(), key=operator.itemgetter(1), reverse=True)
     data_hash = base64.urlsafe_b64encode(md5(str(sorted_value_tuples)).digest())
@@ -24,10 +25,13 @@ def create_graph(data, graph_type='hbar', title=''):
     graph.render_to_png(filename)
     return send_file(filename, mimetype='image/png')
 
-
 @app.route('/trigger', methods=['POST'])
 def trigger():
     return create_graph(request.json, 'hbar', 'Occurrences of Trigger Words')
     
+@app.route('/body_language', methods=['POST'])
+def body_language():
+    return create_graph(request.json, 'pie', 'Body Language Inference')
+ 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
